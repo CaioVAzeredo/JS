7caa3a574aaeb6fb260ded7e8b0463204273698d
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-01-26 21:52:44 -0300

    atividades concluidas

---
+++ cursoJS/aula003/index.js
@@ -0,0 +1,5 @@
+console.log("Caio 'Viana'");
+console.log('Caio "Viana"');
+console.log(`'Caio' "Viana"`);
+
+console.log(35, 15.85, 'Caio Viana')
---
+++ cursoJS/aula004/comentarios.js
@@ -0,0 +1,8 @@
+//um comentário de linha
+console.log('Olá, mundo') //um comentário
+
+/*
+um comentário 
+de 
+bloco
+*/
