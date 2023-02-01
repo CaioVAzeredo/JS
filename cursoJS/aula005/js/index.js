2112677d5bc1f47c8ed2d5021ee28219a4082dc0
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-02-01 20:23:23 -0300

    aula006 feita

---
+++ cursoJS/aula005/js/index.js
@@ -0,0 +1,4 @@
+//comentário
+console.log('Olá, mundo')
+console.log('Esse trecho será exibido no navegador')
+alert('olá, mundo') //alert nao é executado pelo node, apenas pelo console do navegador
---
+++ cursoJS/aula006/index.js
@@ -0,0 +1,10 @@
+let nome = 'Joao'; //String
+
+console.log(nome, ' nasceu em 1998')
+console.log('Em 2000 ', nome, ' conheceu maria')
+
+let Nome // Apenas declaração
+Nome = 'Joao'
+console.log(Nome)
+Nome = 'otávio'
+console.log(Nome)
