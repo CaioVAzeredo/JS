138325347730b746c9a5f8cbda8dca7a958eddb0
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-02-11 11:14:47 -0300

    unidade concluida

--- cursoJS/modulo_1/aula20(exercicio)/js/script.js
+++ cursoJS/modulo_1/aula20(exercicio)/js/script.js
@@ -1,18 +1,26 @@
-function meuEscopo(){
-const form = document.querySelector('.form')
-const resp = document.querySelector('.h2')
+function meuEscopo() {
+    const form = document.querySelector('.form')
+    const resp = document.querySelector('.h2')
 
-const pessoas = []
+    const pessoas = []
 
-function recebeForm(evento){
-    const nome = form.querySelector('.nome')
-    const sobrenome = form.querySelector('.sobrenome')
-    const peso = form.querySelector('.peso')
-    const altura = form.querySelector('.altura')
+    function recebeForm(evento) {
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+        pessoas.push({
+            nome: nome.value,
+            sobrenome:sobrenome.value,
+            peso: Number(peso.value),
+            altura:Number(altura.value),
+        })
+        
+        resp.innerHTML += `<p>Nome: ${nome.value} Sobrenome: ${sobrenome.value} Peso: ${peso.value} Altura: ${altura.value}</p>`
 
-    evento.preventDefault()
-    console.log(nome.value, sobrenome.value, peso.value, altura.value)
-}
-form.addEventListener('submit', recebeForm)
+        console.log(pessoas)
+        evento.preventDefault()
+    }
+    form.addEventListener('submit', recebeForm)
 }
 meuEscopo()
