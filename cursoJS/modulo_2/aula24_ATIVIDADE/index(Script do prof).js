d71372d29696bc82c4bb7cac7ef103dad71c5903
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-02-15 19:24:48 -0300

    atividade feita

--- cursoJS/modulo_2/aula24_ATIVIDADE/index(Script do prof).js
+++ cursoJS/modulo_2/aula24_ATIVIDADE/index(Script do prof).js
@@ -1,6 +1,6 @@
 const frm = document.querySelector('form')
 
-frm.addEventListener('submit', function(e){
+frm.addEventListener('submit', function (e) {
     e.preventDefault()
     const inputPeso = e.target.querySelector('#ipeso')
     const inputAltura = e.target.querySelector('#ialtura')
@@ -8,40 +8,73 @@ frm.addEventListener('submit', function(e){
     const peso = Number(inputPeso.value)
     const alt = Number(inputAltura.value)
 
-    if(!peso){
+    const imc = getImc(peso, alt)
+    const nivelImc = condicao(imc)
+
+    if (!peso) {
         setResultado('PESO INVALIDO', false)
         return
     }
-    if(!alt){
+    if (!alt) {
         setResultado('ALTURA INVALIDA', false)
         return
     }
-    setResultado(getImc(peso, alt))
+    const msg = `seu IMC é ${imc}. (${nivelImc})`
+    setResultado(msg, true)
 
 })
 
+function condicao(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau ', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if (imc >= 39) {
+        return nivel[5]
+    } 
+     if (imc >= 34.9) {
+        return nivel[4]
+    } 
+     if (imc >= 29.9) {
+        return nivel[3]
+    } 
+     if (imc >= 24.9) {
+        return nivel[2]
+    } 
+     if (imc >= 18.5) {
+        return nivel[1]
+    }
+     if(imc < 18.5){
+        return nivel[0]
+    }
+
+}
 
 
 
 
-
-
-function getImc(peso, alt){
+function getImc(peso, alt) {
     let imc = peso / (alt * alt)
-    return imc
+    return imc.toFixed(2)
 }
 
 
 
 
 
-function setResultado(msg, isValid){
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resp')
     resultado.innerHTML = ''
 
     const p = criaP() //pegar informação da function criarP()
     resultado.appendChild(p) //inserir elemento dentro de resultado. Ficaria: <div><p></p></div>
-    p.innerHTML = msg ;
+
+    if(isValid){
+        p.classList.add('bom')
+    }else{
+        p.classList.add('mal')
+    }
+    p.innerHTML = msg;
+    
+    frm.ipeso.focus()
 
 }
 
@@ -49,8 +82,8 @@ function setResultado(msg, isValid){
 
 
 
-function criaP(){
+function criaP() {
     const p = document.createElement('p') //elemento que eu quero criar, nesse caso um <p>
-   /* p.classList.add('pResultado') //adicionar class na tag*/
+    /* p.classList.add('pResultado') //adicionar class na tag*/
     return p
 }
