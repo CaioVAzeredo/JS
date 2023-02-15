7734a588ce5db592ff4e0ceee21bcceca13d75d0
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-02-14 22:58:19 -0300

    atividade feita

---
+++ cursoJS/modulo_2/aula24_ATIVIDADE/script.js
@@ -0,0 +1,45 @@
+function calculoIMC() {
+    const frm = document.querySelector("form")
+    const resp = document.querySelector("#resp")
+
+    function Calculo(e) {
+        const peso = Number(document.querySelector("#ipeso").value)
+        const alt = Number(document.querySelector("#ialtura").value)
+        let imc = peso / (alt * alt)
+
+        if (imc < 18.5) {
+            resp.innerText = `ABAIXO DO PESO`
+            resp.style.color = "rgb(1, 71, 1)"
+            resp.style.background = "rgb(21, 223, 3)"
+        } else if (imc > 18.5 && imc < 24.9) {
+            resp.innerText = `PESO NORMAL`
+            resp.style.color = "rgb(1, 71, 1)"
+            resp.style.background = "rgb(21, 223, 3)"
+        } else if (imc > 25 && imc < 29.9) {
+            resp.innerText = `SOBREPESO`
+            resp.style.color = "#500000"
+            resp.style.background = "#ff0000"
+        } else if (imc > 30 && imc < 34.9) {
+            resp.innerText = `OBESIDADE GRAU 1`
+            resp.style.color = "#500000"
+            resp.style.background = "#ff0000"
+        } else if (imc > 35 && imc < 39.9) {
+            resp.innerText = `OBESIDADE GRAU 2`
+            resp.style.color = "#500000"
+            resp.style.background = "#ff0000"
+        } else if (imc > 40) {
+            resp.innerText = `OBESIDADE GRAU 3`
+            resp.style.color = "#500000"
+            resp.style.background = "#ff0000"
+        }
+
+        frm.ipeso.value = ''
+        frm.ialtura.value = ''
+        frm.ipeso.focus()
+        
+        e.preventDefault()
+
+    }
+    frm.addEventListener('submit', Calculo)
+}
+calculoIMC()
