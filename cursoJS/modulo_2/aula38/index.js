9cb80f2998e2825dcd96abafb1a0d13c08fcb547
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-02-22 18:54:11 -0300

    atividade feita

--- cursoJS/modulo_2/aula38/index.js
+++ cursoJS/modulo_2/aula38/index.js
@@ -1,7 +1,13 @@
 const paragrafos = document.querySelector(".paragrafos")
 const ps = paragrafos.querySelectorAll('p')
 
-const estilosBody = getComputedStyle(document.body) //pegar todos os estilos do body
+/* PARAGRAFO */
+const estilosBody = getComputedStyle(document.body)//pegar todos os estilos do body
 const backgroundColorBody = estilosBody.backgroundColor//pegar a cor do background do body
-console.log(backgroundColorBody)
+
+/* LAÇO DE REPETIÇÃO */
+for (let p of ps) {
+    p.style.background = backgroundColorBody
+    p.style.color = '#ffff'
+}
 
