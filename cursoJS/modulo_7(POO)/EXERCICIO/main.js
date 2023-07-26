7c7390391c033c32b629a3ab73241c4b5417a11e
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-07-26 11:38:59 -0300

    atividade em andamento

---
+++ cursoJS/modulo_7(POO)/EXERCICIO/main.js
@@ -0,0 +1,34 @@
+class ValidaFormulario {
+    constructor() {
+        this.frm = document.querySelector('.form')
+        this.eventos()
+    }
+    eventos() {
+        this.frm.addEventListener('submit', e => {
+            this.handleSubmit(e)
+        })
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        const camposValidos = this.camposSaoValidos()
+    }
+    camposSaoValidos() {
+        let valid = true
+        for(let campo of this.frm.querySelectorAll('.validar')){
+           const label = campo.previousElementSibling.innerHTML //.previousElementSibling pega o elemento anterior (o principal nesse caso é o input e o anterior é o label)
+            if(!campo.value){
+                this.criaErro(campo, `campo ${label} não pode estar em branco`)
+                valid = false
+            }
+        }
+    }
+
+    criaErro(campo, msg){
+        const div = document.createElement('div')
+        div.innerHTML = msg
+        div.classList.add('error-text')
+        campo.insertAdjacentElement('afterend', div)
+    }
+}
+
+const valida = new ValidaFormulario()
