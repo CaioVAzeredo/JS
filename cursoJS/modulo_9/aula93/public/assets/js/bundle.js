fbb47950cf4266aac001fd7972efdb2833378fcb
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-08-01 11:18:11 -0300

    webpack

---
+++ cursoJS/modulo_9/aula93/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//1
+//2
+//3
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
